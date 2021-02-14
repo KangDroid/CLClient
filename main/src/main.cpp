c003746eee1971afd9f9075c80e96afc5f311407
KangDroid <hyunwoo286@gmail.com>
2021-02-14 17:49:19 +0900

    Hook ServerManagement to Main

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -5,12 +5,14 @@
 #include <iostream>
 #include <boost/program_options.hpp>
 
+#include "ServerManagement.h"
 #include "KDRPrinter.h"
 
 using namespace std;
 using namespace boost::program_options;
 
 int main(int argc, char** argv) {
+    ServerManagement server_management;
     variables_map vm;
     options_description desc("Supported Options/Args");
     desc.add_options()
@@ -20,10 +22,18 @@ int main(int argc, char** argv) {
         store(parse_command_line(argc, argv, desc), vm);
     } catch (const exception& expn) {
         KDRPrinter::print_error(string(expn.what()));
+        return 1;
     }
 
     if (vm.empty() || vm.count("help")) {
         cout << desc << endl;
+    }
+
+    if (vm.contains("master-ip")) {
+        server_management.server_base_url = vm["master-ip"].as<string>();
+    } else {
+        KDRPrinter::print_error("This Program needs Master-IP to continue!");
+        return 1;
     }
     return 0;
 }
