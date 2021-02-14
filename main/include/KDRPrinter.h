92c0371f5cfc4c7eba6fef9779c20ddfc8134191
KangDroid <hyunwoo286@gmail.com>
2021-02-14 20:00:14 +0900

    KDRPrinter: Add Verbose Printer

--- main/include/KDRPrinter.h
+++ main/include/KDRPrinter.h
@@ -10,6 +10,7 @@
 
 #define RESET   "\033[0m"
 #define RED     "\033[31m"
+#define GREEN   "\033[32m"
 
 using namespace std;
 
@@ -18,6 +19,9 @@ public:
     static void print_error(string& error_message);
     static void print_error(const string& error_message);
     static void print_error(vector<string>& error_message);
+    static void print_verbose(string& verbose_message);
+    static void print_verbose(const string& verbose_message);
+    static void print_verbose(vector<string>& verbose_message);
 };
 
 
--- main/src/KDRPrinter.cpp
+++ main/src/KDRPrinter.cpp
@@ -17,3 +17,17 @@ void KDRPrinter::print_error(vector<string> &error_message) {
         cerr << RED << "[Error]: " << er_message << RESET << endl;
     }
 }
+
+void KDRPrinter::print_verbose(string &verbose_message) {
+    cout << GREEN << "[Verbose]: " << verbose_message << RESET << endl;
+}
+
+void KDRPrinter::print_verbose(const string &verbose_message) {
+    cout << GREEN << "[Verbose]: " << verbose_message << RESET << endl;
+}
+
+void KDRPrinter::print_verbose(vector<string> &verbose_message) {
+    for (string vm : verbose_message) {
+        cout << GREEN << "[Verbose]: " << vm << RESET << endl;
+    }
+}
