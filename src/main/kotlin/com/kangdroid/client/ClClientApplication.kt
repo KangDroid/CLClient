1dc47933289aba28a665eff07b64be9a2afe6b0a
KangDroid <hyunwoo286@gmail.com>
2021-03-01 21:27:22 +0900

    Revert "Main: Disable Runing SpringBootApplication for now"
    This reverts commit 7aed269df06b57dc150a61549e64c73323e098f0.

--- src/main/kotlin/com/kangdroid/client/ClClientApplication.kt
+++ src/main/kotlin/com/kangdroid/client/ClClientApplication.kt
@@ -1,4 +1,11 @@
 package com.kangdroid.client
 
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClClientApplication
+
 fun main(args: Array<String>) {
+    runApplication<ClClientApplication>(*args)
 }
