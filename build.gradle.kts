9b91924dcf4f6956953490d171747d7149a8388f
KangDroid <hyunwoo286@gmail.com>
2021-03-01 21:28:45 +0900

    Gradle: Add JUnit4

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation(kotlin("test-junit"))
 }
 
 tasks.withType<KotlinCompile> {
@@ -77,6 +78,6 @@ tasks.withType<KotlinCompile> {
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+	useJUnit()
 	finalizedBy("jacocoTestReport")
 }
