221f4cfbb029e0c97d5fd6abf1194beab865866f
KangDroid <hyunwoo286@gmail.com>
2021-03-01 20:53:02 +0900

    Add Initial Project

---
+++ src/main/kotlin/com/kangdroid/client/ClClientApplication.kt
@@ -0,0 +1,11 @@
+package com.kangdroid.client
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClClientApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClClientApplication>(*args)
+}
---
+++ src/test/kotlin/com/kangdroid/client/ClClientApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kangdroid.client
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClClientApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
