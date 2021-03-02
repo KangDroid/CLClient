b6076fa56cb79d6d001cd756c5efe2bbcf12221d
KangDroid <hyunwoo286@gmail.com>
2021-03-02 23:58:37 +0900

    Gradle: Fix jacoco is false-recognizing automatically generated class

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,8 @@ tasks.jacocoTestCoverageVerification {
 
             excludes = listOf(
                 "com.kangdroid.client.ClClientApplicationKt",
-                "com.kangdroid.client.ClClientApplication"
+                "com.kangdroid.client.ClClientApplication",
+                "com.kangdroid.client.communication.ServerCommunication.*"
             )
         }
     }
