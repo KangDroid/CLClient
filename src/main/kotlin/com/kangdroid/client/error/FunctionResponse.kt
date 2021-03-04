a3b0bc0fb9a2b16545f9d6e11b7327535465958e
KangDroid <hyunwoo286@gmail.com>
2021-03-04 23:38:35 +0900

    FunctionResponse: Add Response ENUM Class value for make other service function to be more specific

---
+++ src/main/kotlin/com/kangdroid/client/error/FunctionResponse.kt
@@ -0,0 +1,11 @@
+package com.kangdroid.client.error
+
+enum class FunctionResponse {
+    // Success
+    SUCCESS,
+    // Fail With Reason
+    CLIENT_NO_TOKEN,
+    SERVER_COMMUNICATION_FAILED_WITH_4XX_5XX,
+    SERVER_RESPONSE_OK_BUT_NO_BODY,
+    SERVER_RESPONSE_OK_BUT_WRONG_FORMAT
+}
